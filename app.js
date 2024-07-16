1e675129f3ecd720da1bd952ff23340bb5f87a69
ecem <ecemtufekci@hotmail.com>
2024-07-16 09:31:55 +0300

    final

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ const toggle = document.querySelector(".toggle");
 const quit = document.querySelector(".quit");
 const sidebar = document.querySelector(".sidebar");
 
-toggle.addEventListener
+toggle.addEventListener("click", function (){
+    sidebar.classList.toggle('show-sidebar');
+
+});
+
+quit.addEventListener("click", function(){
+    sidebar.classList.remove('show-sidebar');
+});
